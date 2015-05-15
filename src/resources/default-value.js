1c2a3bdb5866dcd18e63ea83761cff8044f91cfb
jadrake75 <jadrake75@gmail.com>
2015-05-15 10:48:38 -0400

    removed files

--- src/resources/default-value.js
+++
@@ -1,9 +0,0 @@
-import {valueConverter} from 'aurelia-framework';
-
-@valueConverter("defaultValue")
-export class defaultValueConverter {
-
-	toView(value, defValue) {
-		return ( !value ) ? defValue : value ;
-	}
-}
--- src/resources/index.js
+++
@@ -1,3 +0,0 @@
-export function install(aurelia) {
-	aurelia.globalizeResources('./default-value');
-}
