890823810ea39548a9ef058601acba6b97643c07
jadrake75 <jadrake75@gmail.com>
2015-07-31 18:16:58 -0400

    Fix lack of translate

--- build/tasks/translate-extract.js
+++ build/tasks/translate-extract.js
@@ -1,19 +1,18 @@
 var gulp = require('gulp');
-var i18next = require('aurelia-i18next-parser');
+
+//var i18next = require('aurelia-i18next-parser');
 var paths = require('../paths');
 
-console.log(i18next);
-
-gulp.task('i18n', function() {
-    gulp.src(paths.root + '**/*')
-        .pipe(i18next.i18next({
-            routesModuleId: "app",          //module to extract routes from
-            appPath: "src",                    //path to the aurelia application files relative from the gulpfile
-            locales: ['en', 'de'],             //translation files will be created for these
-            defaultLocale: 'en',               //this will be treated as the default locale, the extracted values will not be transformed for this locale
-            translation_attribute:"i18n",      //attribute that is used in the html to specify translation keys
-            functions:['t'],                   //function that is used in javascript to translate values
-            defaultNamespace:'translation'
-        }))
-        .pipe(gulp.dest(paths.locale));
-});
+//gulp.task('i18n', function() {
+//    gulp.src(paths.root + '**/*')
+//        .pipe(i18next.i18next({
+//            routesModuleId: "app",          //module to extract routes from
+//            appPath: "src",                    //path to the aurelia application files relative from the gulpfile
+//            locales: ['en', 'de'],             //translation files will be created for these
+//            defaultLocale: 'en',               //this will be treated as the default locale, the extracted values will not be transformed for this locale
+//            translation_attribute:"i18n",      //attribute that is used in the html to specify translation keys
+//            functions:['t'],                   //function that is used in javascript to translate values
+//            defaultNamespace:'translation'
+//        }))
+//        .pipe(gulp.dest(paths.locale));
+//});
