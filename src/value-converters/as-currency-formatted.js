da26696244ba373cb3c8bd32f311ca435ee34b77
jadrake75 <jadrake75@gmail.com>
2015-12-22 00:51:05 -0500

    Additional handling for 0 values
    #2

--- src/value-converters/as-currency-formatted.js
+++ src/value-converters/as-currency-formatted.js
@@ -20,8 +20,11 @@ import {valueConverter} from 'aurelia-framework';
 export class asCurrencyValueConverter {
 
     toView(value, currency) {
-        if (value && currency) {
-            return (value).toLocaleString("en", {style: "currency", currency: currency, minimumFractionDigits: 2});
+        if (typeof value !== 'undefined' && currency) {
+            if( +value <= 0 ) {
+                return "-";
+            }
+            return value.toLocaleString("en", {style: "currency", currency: currency, minimumFractionDigits: 2});
         }
         return value;
     }
