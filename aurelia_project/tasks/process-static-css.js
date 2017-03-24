021345e834f4998badbaf9f7425a892ac50b4287
jadrake75 <jadrake75@gmail.com>
2017-03-24 14:16:21 -0400

    Fix splash screen processing from LESS to SASS
    #140

--- aurelia_project/tasks/process-static-css.js
+++ aurelia_project/tasks/process-static-css.js
@@ -1,11 +1,22 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import changedInPlace from 'gulp-changed-in-place';
 import cleanCSS from 'gulp-clean-css';
 import project from '../aurelia.json';
+import path from 'path';
 
 export function processStaticCSS() {
+
+    let bootstrap = path.join(process.cwd(), 'node_modules', 'bootstrap', 'scss');
+    let theme = path.join(process.cwd(), 'src', 'theme');
+
     return gulp.src(project.staticCSS.source)
-        .pipe(sass())
+        .pipe(sass({
+            includePaths: [
+                bootstrap,
+                theme
+            ]
+        }).on('error', sass.logError))
         .pipe(cleanCSS())
         .pipe(gulp.dest(project.paths.staticCSS));
 
