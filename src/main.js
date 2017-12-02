c6f06bb6eb3f1542bb1566933ec5fab5c6f592e1
jadrake75 <jadrake75@gmail.com>
2017-12-02 03:13:03 -0500

    Initialize Popper.js

--- src/main.js
+++ src/main.js
@@ -81,9 +81,10 @@ export function configure(aurelia) {
 }
 
 function configureGlobalLibraries() {
-    require(['tether', 'jquery'], (tether, jquery) => {
+    require(['tether', 'jquery', 'popper.js'], (tether, jquery, popper) => {
         window.Tether = tether;
         window.jQuery = jquery;
+        window.Popper = popper;
         require(['bootstrap']);
     });
 }
