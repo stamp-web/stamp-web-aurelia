00bf64fea4c960612dfc56fb59b186cf416a5663
jadrake75 <jadrake75@gmail.com>
2023-10-08 21:36:00 -0400

    Fix jest + jsdom issue
    * Updated jest-pretest.js to match example from stamp-imageparsing where this issue was addressed
    
    Fixes #197

--- test/jest-pretest.js
+++ test/jest-pretest.js
@@ -1,6 +1,9 @@
+import "isomorphic-fetch";
+import path from 'path';
 import 'aurelia-polyfills';
 import {Options} from 'aurelia-loader-nodejs';
-import {globalize} from 'aurelia-pal-nodejs';
-import path from 'path';
+
+import {initialize} from 'aurelia-pal-browser';
+initialize();
+
 Options.relativeToDir = path.join(__dirname, 'unit');
-globalize();
